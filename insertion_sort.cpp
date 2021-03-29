d3813248818998591037d0e1de248e56c3757d14
Bane34 <bane34.42@gmail.com>
2021-03-30 00:31:10 +0200

    initial commit

---
+++ insertion_sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[6] = {0, 6, 5, 10, 9, 0};
+
+    int key;
+    int j, i;
+
+    for(j = 1; j < 6; j++) {
+        key = arr[j];
+        i = j - 1;
+
+        while(i >= 0 && arr[i] > key){
+            arr[i + 1] = arr[i];
+            i = i - 1;
+        }
+
+        arr[i + 1] = key;
+    }
+
+    return 0;
+}
