42f0239cec31379d2cd26e350868155b7f302a1a
Bane34 <bane34.42@gmail.com>
2021-08-21 23:54:22 +0200

    inital commit

---
+++ maths/reverse.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define LOG(x) std::cout << x << '\n';
+
+// Algorithm to reverse an integer
+// Example: reverse(504) -> return 405
+
+int reverse(int n);
+
+int main(void){
+
+    LOG(reverse(1534236469));
+
+    return 0;
+}
+
+int reverse(int n){
+    int reversed = 0;
+
+    while(n != 0) {
+        reversed += (n % 10);
+        n /= 10;
+        reversed *= 10;
+        
+    }
+
+    return reversed / 10;
+}
